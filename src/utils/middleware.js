f66f27bee22ff330a9727052928a3f84f85f0a54
gonzalo00913 <gonzalo-masa@outlook.com>
2024-02-03 12:40:01 -0300

    Levantar servidor

---
+++ src/utils/middleware.js
@@ -0,0 +1,13 @@
+const logger = require("./logger");
+
+const requestLogger = (request, response, next) => {
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
+
+module.exports = {
+  requestLogger,
+};
