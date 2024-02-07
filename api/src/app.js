e987329557a7648b381788fc372e09ee0ebc18a2
gonzalo00913 <gonzalo-masa@outlook.com>
2024-02-07 04:58:05 -0300

    Modelo usuario y relación

--- api/src/app.js
+++ api/src/app.js
@@ -4,21 +4,25 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+
 const dishRouter = require("./controllers/dish");
+const userRouter = require("./controllers/user")
 
 mongoose
   .connect(config.MONGO_URI)
   .then(() => {
-    logger.info("✔ connected database");
+    logger.info("✅ connected database");
   })
   .catch((error) => {
-    logger.error("✖ Error connecting database", error.message);
+    logger.error("❌ Error connecting database", error.message);
   });
 
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+
 app.use("/api/dish", dishRouter);
+app.use("/api/user", userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
