aead55ef9b1f5020cf7cf71a1395b060f33821c3
nicoflorentin <nicogrunger@hotmail.com>
2024-02-09 14:55:38 -0300

    configurar redux toolkit

---
+++ client/src/views/Main/components/Main.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react"
+import { fetchDishes } from "../../../redux/slices/dishesSlice"
+import { increment } from "../../../redux/slices/counterSlice"
+
+import { useDispatch, useSelector } from "react-redux"
+
+const Main = () => {
+	const dispatch = useDispatch()
+	const { loading, error, data } = useSelector(state => state.dishes) // Usa el slice para los platos
+
+	console.log(data)
+
+	useEffect(() => {
+		dispatch(fetchDishes())
+	}, [dispatch])
+
+	const renderDishes = data && data.map(dish => <div>{dish.title}</div>)
+
+	return <div>
+		<h1>Dishes</h1>
+		<ul>
+			{loading ? <p>loading</p> : renderDishes}
+		</ul>
+	</div>
+}
+
+export default Main
