12bf8096448028cd7584bd51ab085995cad90f47
nicoflorentin <nicogrunger@hotmail.com>
2024-04-23 09:30:36 -0300

    styles : add favicon

---
+++ client/src/components/WaitingGuy/WaitingGuy.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const WaitingGuy = () => {
+
+
+	return (
+		<img className="w-96 h-40 " src="https://www.troublefreepool.com/media/mr-bean-waiting-gif.190931/full" alt="" />
+	)
+}
+
+export default WaitingGuy
