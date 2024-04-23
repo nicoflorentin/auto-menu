876ba45ece0f8be20628d4f47cc40ee56ea338ef
nicoflorentin <nicogrunger@hotmail.com>
2024-04-23 09:59:38 -0300

    styles : add favicon

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -3,7 +3,7 @@ import withAuth from "./withAuth"
 import { Input, Button, Card, CardBody, Tabs, Tab } from "@nextui-org/react"
 import Spinner from "../../components/Spinner"
 import { Link, useNavigate } from "react-router-dom"
-import logo from 'assets/page-logo.png'
+import logo from 'assets/img/page-logo.png'
 import YoutubeFrame from "components/YoutubeFrame/YoutubeFrame"
 import WaitingGuy from "components/WaitingGuy/WaitingGuy"
 
